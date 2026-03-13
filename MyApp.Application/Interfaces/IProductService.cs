8d663c27b265af2121ea446579748574937f185d
Toan Pham Duc <toan.pham@orientsoftware.com>
2026-03-13 15:39:19 +0700

    add new request DTO

--- MyApp.Application/Interfaces/IProductService.cs
+++ MyApp.Application/Interfaces/IProductService.cs
@@ -8,8 +8,11 @@ namespace MyApp.Application.Interfaces
     {
         Task<List<ProductResponseDto>> GetAllProductsAsync();
         Task<ProductResponseDto> GetProductByIdAsync(int id);
-        Task<ProductResponseDto> AddProductAsync(ProductRequestDto request, IFormFile? image, CancellationToken ct = default);
-        Task<bool> UpdateProductAsync(int id, ProductRequestDto request, IFormFile? newImage, CancellationToken ct = default);
+
+        Task<ProductResponseDto> AddProductAsync(CreateProductRequestDto request, CancellationToken ct = default);
+
+        Task<bool> UpdateProductAsync(int id, UpdateProductRequestDto request, CancellationToken ct = default);
+
         Task<bool> DeleteProductAsync(int id);
     }
 }
